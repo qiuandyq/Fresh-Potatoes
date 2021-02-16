e01ea183153899740befe4c2f0275f6e768a3dc0
Andy <andy.qiu.you@gmail.com>
2021-02-15 22:18:05 -0800

    create register branch

--- fresh-potatos/wsgi.py
+++ fresh-potatos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Fresh_Potatos project.
+WSGI config for fresh-potatos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fresh_Potatos.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fresh-potatos.settings')
 
 application = get_wsgi_application()
