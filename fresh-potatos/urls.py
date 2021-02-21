537233dbef74f435f3bef1d784205c521545161d
mirandar <miranda_rogers@sfu.ca>
2021-02-20 17:46:05 -0800

    added a forgot password path

--- fresh-potatos/urls.py
+++ fresh-potatos/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('survey/', user_views.survey, name="survey"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"), # 'LoginViews' is a class based view
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"), # built in views handles forms and logic for us. (not the templates)
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name="password_reset"), # 
     path('', include('homepage.urls')),
 ]
