60bf70c215612b67b93c93bfb2e4be9ede0ea188
mirandar <miranda_rogers@sfu.ca>
2021-02-20 17:02:56 -0800

    added login and logout path. Also imported auth_views

--- fresh-potatos/urls.py
+++ fresh-potatos/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views # import django's deafault backend authentication 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
     path('profile/', user_views.profile, name="profile"),
     path('survey/', user_views.survey, name="survey"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"), # 'LoginViews' is a class based view
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"), # built in views handles forms and logic for us. (not the templates)
     path('', include('homepage.urls')),
 ]
