10af754cb755592d4f18463078c10ffe32c57217
Andy <andy.qiu.you@gmail.com>
2021-03-08 21:41:58 -0800

    added new field to profile model db

--- users/forms.py
+++ users/forms.py
@@ -14,8 +14,30 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 class ProfileUpdateForm(forms.ModelForm):
+    GENRES = (
+        ('28', 'Action'),
+        ('12', 'Adventure'),
+        ('16', 'Animation'),
+        ('35', 'Comedy'),
+        ('80', 'Crime'),
+        ('99', 'Documentary'),
+        ('18', 'Drama'),
+        ('10751', 'Family'),
+        ('14', 'Fantasy'),
+        ('36', 'History'),
+        ('27', 'Horror'),
+        ('10402', 'Music'),
+        ('9648', 'Mystery'),
+        ('10749', 'Romance'),
+        ('878', 'Science Fiction'),
+        ('10770', 'TV Movie'),
+        ('53', 'Thriller'),
+        ('10752', 'War'),
+        ('37', 'Western'),
+    )
+
     dob = forms.DateField(label="Date of Birth", widget=forms.widgets.DateTimeInput(attrs={"type": "date"}))
-    
+    genre = forms.MultipleChoiceField(label="Genre", widget=forms.CheckboxSelectMultiple, choices=GENRES)
     class Meta:
         model = Profile
-        fields = ['dob']
+        fields = ['dob', 'genre']
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     dob = models.DateField("Date of Birth", blank=True, null=True)
+    genre = models.CharField(max_length=300, default="")
 
     def __str__(self):
         return f'{self.user.username} Profile'
