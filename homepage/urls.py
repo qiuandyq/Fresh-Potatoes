fc407ae0792aa9a9243c1a5b961f37013e69b103
gza30 <mike_zhao@sfu.ca>
2021-03-24 14:23:35 -0700

    Replace urls.py

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('FAQ/', views.faq, name="faq"), # add in FAQ page path
     path('farm/', views.farm, name="farm"),
+    path('farmedit/', views.farmedit, name="farmedit"),
     path('more_info/<str:media_type>/<int:movie_id>/', views.more_info, ), # the <> brackets means the movie_num can be dynamic like a variable. Our int: means our input must be a string
 ]
