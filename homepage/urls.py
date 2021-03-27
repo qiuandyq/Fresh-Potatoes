b30f2944eaa360bb40602a8cb652f323919518e9
mirandar <miranda_rogers@sfu.ca>
2021-03-26 23:01:43 -0700

    fixed add to liked list redirect and got it working for TV shows

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name="about"),
     path('FAQ/', views.faq, name="faq"), # add in FAQ page path
     path('more_info/<str:media_type>/<int:movie_id>/', views.more_info, ), # the <> brackets means the movie_num can be dynamic like a variable. Our int: means our input must be a string
+    path('added_done/<str:media_type>/<int:movie_id>/', views.movie_added, name="added_movie_done"),
 ]
